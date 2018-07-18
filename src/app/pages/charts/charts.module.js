80266ebfee7fb6e08bbfb111e252ae9febf80cab
premsrepo <premgowdabp@gmail.com>
2018-07-18 17:49:09 +0530

    menu items added back to sidebar, datepicker added

--- src/app/pages/charts/charts.module.js
+++ src/app/pages/charts/charts.module.js
@@ -14,17 +14,17 @@
 
   /** @ngInject */
   function routeConfig($stateProvider) {
-    // $stateProvider
-    //     .state('charts', {
-    //       url: '/charts',
-    //       abstract: true,
-    //       template: '<div ui-view  autoscroll="true" autoscroll-body-top></div>',
-    //       title: 'Charts',
-    //       sidebarMeta: {
-    //         icon: 'ion-stats-bars',
-    //         order: 150,
-    //       },
-    //     });
+    $stateProvider
+        .state('charts', {
+          url: '/charts',
+          abstract: true,
+          template: '<div ui-view  autoscroll="true" autoscroll-body-top></div>',
+          title: 'Charts',
+          sidebarMeta: {
+            icon: 'ion-stats-bars',
+            order: 150,
+          },
+        });
   }
 
 })();
